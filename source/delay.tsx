cc58bd6e9372e3d84ade85017c8df63f2e82e621
Zgrskd17 <164410015+Zgrskd17@users.noreply.github.com>
2024-03-24 12:26:48 +0000

    TS&REACT

---
+++ source/delay.tsx
@@ -0,0 +1,7 @@
+export default function delay(time: number): Promise<(resolve?: () => {}, reject?: () => {}) => {}> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+}
